f63eab2c96c62fd854537acc0e6625cbef198a9f
jeongwookim <kjw6235@naver.com>
2022-11-23 17:50:10 +0900

    study express, cookie

---
+++ Ex08login/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const loginRouter = require('./routes/login');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+app.set('port', process.env.PORT||8888);
+app.set('view engine', 'html'); //넌적스 = njk / html      
+app.use(bodyParser.urlencoded({extended:true})); 
+app.use('/user', loginRouter);
+
+
+nunjucks.configure('views',{
+    express: app, //app 객체 연결
+    watch : true, //html 파일이 연결되면 템플릿 엔진을 다시 렌더링
+})
+app.listen(app.get('port'), ()=>{
+    console.log(app.get('port'), '번 포트에서 서버 연결 대기중 ...');
+});
