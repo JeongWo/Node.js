69a29d9a665888839556d564cee8d5f882d13fbc
jeongwookim <kjw6235@naver.com>
2022-11-25 17:50:13 +0900

    ORM Commit

---
+++ Ex11ORM/models/index.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+const User = require('./user');
+const env = 'development';
+const config = require('../config/config.json')[env];
+
+// db연결 정보
+const sequelize = new Sequelize(config.database,config.username, config.password, config);
+
+const db = {}; //squelize, object;
+
+// key       //value(db정보)
+db.sequelize = sequelize;
+
+
+db.User = User; //db와 테이블 연결
+
+User.init(sequelize);// table 초기화
+User.associate(db); // 관계 설정
+
+module.exports = db;
